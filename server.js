031b32b2a7fdbe2164bd9e0a9768fe9ec808f94b
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-20 10:46:01 -0300

    Criação do objeto de conexão com mongoDB e teste com o método POST.

--- server.js
+++ server.js
@@ -4,10 +4,33 @@ const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
 // start do módulo express
 const app = express();
-// configurando middlware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// configurando middlware body-parser
+app.use(bodyParser.urlencoded({ extended: true })); // recebendo dados via url
+app.use(bodyParser.json()); // recebendo dados no formato json
 // detalhes do servidor
 const port = 3000;
 
 app.listen(port, () => console.log('Servidor ON'));
+
+// objeto de conexão com mongodb
+const db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}), {}
+);
+
+// api
+app.post('/api', (req, res) => {
+    let dados = req.body;
+    db.open((err, mongoclient) => {
+        mongoclient.collection('postagens', (err, collection) => {
+            collection.insert(dados, (err, records) => {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(records);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
